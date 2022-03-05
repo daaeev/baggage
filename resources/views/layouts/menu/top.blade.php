3d60048bf48a12fc89bcfe3a6508be79061b7d13
daaeev <LfdblGbhju@gmail.com>
2022-03-05 16:54:18 +0200

    Убрано поле csrf-токена в форме поиска

--- resources/views/layouts/menu/top.blade.php
+++ resources/views/layouts/menu/top.blade.php
@@ -24,7 +24,6 @@
     <div class="search-form ml-auto">
         <div class="form-w3layouts-grid">
             <form action="{{route('catalog')}}" method="get" class="newsletter">
-                @csrf
                 <input class="search" type="search" placeholder="Search here..." required name="search" maxlength="25">
                 <button class="form-control btn" value=""><span class="fa fa-search"></span></button>
             </form>
