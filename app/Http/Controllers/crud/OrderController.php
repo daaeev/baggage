ebb1ac770827854ed1ae9674519adcc90f777a56
daaeev <LfdblGbhju@gmail.com>
2022-03-07 12:18:25 +0200

    Добавлены комментарии к методу OrderController::declineOrder()

--- app/Http/Controllers/crud/OrderController.php
+++ app/Http/Controllers/crud/OrderController.php
@@ -8,15 +8,25 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    /**
+     * Метод отвечает за отклонение заказа (его удаление из таблицы 'orders')
+     *
+     * @param Request $request
+     * @param OrdersRepositoryInterface $ordersRepository
+     * @return mixed
+     */
     public function declineOrder(Request $request, OrdersRepositoryInterface $ordersRepository)
     {
+        // Валидация данных
         $request->validate([
             'order_id' => 'required|exists:\App\Models\Order,id',
         ]);
 
+        // Получение экземпляра заказа
         $order_id = $request->input('order_id');
         $order = $ordersRepository->getFistOrNull($order_id);
 
+        // Удаление данных из БД
         if (!$order->delete()) {
             $request->session()->flash('status_failed', "Order decline with id $order_id failed");
 
